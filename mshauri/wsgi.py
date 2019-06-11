85e7bb21a3e0e3f123d1511fe8336625404a8ad1
lizKimita <kimita.wanjiku@gmail.com>
2019-06-11 17:13:01 +0300

    initial project structure

---
+++ mshauri/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for mshauri project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mshauri.settings")
+
+application = get_wsgi_application()
