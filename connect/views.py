25c61085912e961cbaeb0d0e25400aa903493e62
saudahabib <saudababs00@gmail.com>
2019-06-11 18:12:30 +0300

    successfully displayed one page

--- connect/views.py
+++ connect/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http  import HttpResponse
 
 # Create your views here.
+def welcome(request):
+    return HttpResponse("Now you see me now you don't")
