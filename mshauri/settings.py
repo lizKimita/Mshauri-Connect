69d0dec8c13065039c5ede1ec3763e3888454b90
lizKimita <kimita.wanjiku@gmail.com>
2019-06-12 11:08:18 +0300

    models merging

--- mshauri/settings.py
+++ mshauri/settings.py
@@ -81,6 +81,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'connect',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -120,8 +122,10 @@ WSGI_APPLICATION = 'mshauri.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mshauri',
+        'USER': 'antavio',
+        'PASSWORD':'antavio',
     }
 }
 
